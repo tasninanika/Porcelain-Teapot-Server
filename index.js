d045735f275133613afb77656ce1575660061389
anikatasnin <jarintasnin09@gmail.com>
2025-05-21 18:27:38 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// teaMaster
-// wZ3ofIgAuvzwyL7R
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
-const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
