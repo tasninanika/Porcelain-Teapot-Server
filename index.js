5f2710422954538d5d3cdce2f0b8efcaf0de2101
anikatasnin <jarintasnin09@gmail.com>
2025-05-26 19:50:37 +0600

    get users from the database

--- index.js
+++ index.js
@@ -87,6 +87,12 @@ async function run() {
     });
 
     // users related apis
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const newUser = req.body;
       console.log("creating new user");
