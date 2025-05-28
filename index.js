a4386b94b2664800b15ca449617d50e75d168eb9
anikatasnin <jarintasnin09@gmail.com>
2025-05-28 14:45:01 +0600

    delete users from the database

--- index.js
+++ index.js
@@ -100,6 +100,11 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
