988df56bd81b6bce7c508c46defdc50ce7ac661e
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 13:59:24 +0600

    server is ready

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hot Coffee Server is Boiling!!!");
+  res.send("Tea Server is Cooking!!!");
 });
 
 // MongoDB Connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63zdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,39 +29,37 @@ async function run() {
   try {
     await client.connect();
     // Creating Database
-    const coffeesCollection = client
-      .db("EspressoEmporium")
-      .collection("coffees");
+    const teasCollection = client.db("PorcelainTea").collection("teas");
 
     // Read Coffees
-    app.get("/coffees", async (req, res) => {
-      const data = await coffeesCollection.find().toArray();
+    app.get("/teas", async (req, res) => {
+      const data = await teasCollection.find().toArray();
       res.send(data);
     });
 
-    // Read Single Coffee
-    app.get("/coffees/:id", async (req, res) => {
+    // Read Single Tea
+    app.get("/teas/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const result = await coffeesCollection.findOne(filter);
+      const result = await teasCollection.findOne(filter);
       console.log(result);
       res.send(result);
     });
 
-    // Create Coffees
-    app.post("/coffees", async (req, res) => {
+    // Create Teas
+    app.post("/teas", async (req, res) => {
       const data = req.body;
-      const result = await coffeesCollection.insertOne(data);
+      const result = await teasCollection.insertOne(data);
       res.send(result);
     });
 
-    // Update Coffee
-    app.put("/coffees/:id", async (req, res) => {
+    // Update Tea
+    app.put("/teas/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      const updatedCoffee = {
+      const updatedTea = {
         $set: {
           name: data.name,
           chef: data.chef,
@@ -73,19 +71,19 @@ async function run() {
           price: data.price,
         },
       };
-      const result = await coffeesCollection.updateOne(
+      const result = await teasCollection.updateOne(
         filter,
-        updatedCoffee,
+        updatedTea,
         options
       );
       res.send(result);
     });
 
-    // Delete a Coffee
-    app.delete("/coffees/:id", async (req, res) => {
+    // Delete a Tea
+    app.delete("/teas/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const result = await coffeesCollection.deleteOne(filter);
+      const result = await teasCollection.deleteOne(filter);
       res.send(result);
     });
   } finally {
